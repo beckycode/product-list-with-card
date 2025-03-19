0f57fb018e63d345400cbc0dcf121e3449457e3d
Rebeca González <hi.beckycode@gmail.com>
2025-03-18 23:24:49 -0400

    add localStorage for persisting data

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,19 +2,26 @@ import { defineStore } from 'pinia'
 
 export const useCart = defineStore('cart', {
   state: () => {
+    // Load initial state from localStorage
+    const savedCart = localStorage.getItem('cart')
     return {
-      cart: [],
+      cart: savedCart ? JSON.parse(savedCart) : [],
     }
   },
   actions: {
+    saveToStorage() {
+      localStorage.setItem('cart', JSON.stringify(this.cart))
+    },
     addItem(item) {
       item = { ...item, quantity: 1 }
       this.cart.push(item)
+      this.saveToStorage()
     },
     incrementItem(item) {
       const cartItem = this.cart.find((cartItem) => cartItem.id === item.id)
       if (cartItem) {
         cartItem.quantity++
+        this.saveToStorage()
       }
     },
     decrementItem(item) {
@@ -25,9 +32,11 @@ export const useCart = defineStore('cart', {
         // Remove item if quantity would become 0
         this.cart = this.cart.filter((cartItem) => cartItem.id !== item.id)
       }
+      this.saveToStorage()
     },
     removeItem(itemId) {
       this.cart = [...this.cart.filter((cartItem) => cartItem.id !== itemId)]
+      this.saveToStorage()
     },
   },
   getters: {
